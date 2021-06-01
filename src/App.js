b988a4c5bec9a00ad32a5d37faff645cb1206fc3
Your Name <you@example.com>
2021-06-01 17:48:10 +0530

    Second Deployment

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import Navbar from "./component/Navbar"
 import Carousel from "./component/Carousel"
 import Login from "./component/Login"
 import Map   from "./component/Map"
+import {useState} from "react"
 
 var details = {
   projectname:"technical shop",
   projectdetails:"related to tech gadegts"
 }
-
 function App() {
+  var [login,setLogin] = useState(false)
+
+  var callme = ()=> {
+  setLogin(true )
+  }
+
   return (
     <div>
-    <Navbar details= {details}></Navbar>
+    <Navbar  isloggin={login} details= {details}></Navbar>
     <Carousel></Carousel>
+    <Login  call={callme} />
     <Map />
     </div>
   );
